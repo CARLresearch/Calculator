1a1f35704789e4ced78b5dcde80b40c412d94b96
Nachiketa Barve <55313060+NachiBarve@users.noreply.github.com>
2019-10-03 20:29:09 +0530

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -16,3 +16,47 @@ int add()
     }
     cout<<"Sum is "<<sum;
 }
+                                              
+int subtract()
+{
+    int nos[20],i,limit,diff=0;
+    cout<<"How many nos to be subtracted-";
+    cin>>limit;
+    cout<<"Enter the nos-";
+    for(i=1;i<=limit;i++)
+    {
+        cin>>nos[i];
+        diff=nos[i]-diff;
+    }
+    cout<<"Difference is "<<diff;
+}
+
+int multiply()
+{
+    int nos[20],i,limit,prod=1;
+    cout<<"How many nos to be multiplied-";
+    cin>>limit;
+    cout<<"Enter the nos-";
+    for(i=1;i<=limit;i++)
+    {
+        cin>>nos[i];
+        prod=prod*nos[i];
+    }
+    cout<<"Product is "<<prod;
+}
+
+int divide()
+{
+    int nos[20],i,limit;
+    float quo=1;
+    cout<<"How many nos to be divided-";
+    cin>>limit;
+    cout<<"Enter the nos-";
+    for(i=1;i<=limit;i++)
+    {
+        cin>>nos[i];
+        quo=nos[i]/quo;
+    }
+    cout<<"Quotient is "<<quo;
+}
+
